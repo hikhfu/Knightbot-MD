d53ba4cc38ccfc5c41846d11638c6f06c4327f49
hikhfu <efootballhakimi3@gmail.com>
2025-03-25 12:18:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Hakimi',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Figfo Bot",
+  botOwner: 'Hakimi', // Your name
+  ownerNumber: '+252611533130', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
